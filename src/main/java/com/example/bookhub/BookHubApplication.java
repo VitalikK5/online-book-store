7097703d2b3c78dbf0743de85a84a2d92e70ceaa
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-10 01:35:58 +0200

    Finish hw3

--- src/main/java/com/example/bookhub/BookHubApplication.java
+++ src/main/java/com/example/bookhub/BookHubApplication.java
@@ -8,5 +8,4 @@ public class BookHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookHubApplication.class, args);
     }
-
 }
--- src/main/java/com/example/bookhub/model/Book.java
+++ src/main/java/com/example/bookhub/model/Book.java
@@ -9,11 +9,16 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
 
 @Entity
 @Getter
 @Setter
 @Table(name = "books")
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted = false")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +39,9 @@ public class Book {
     private String description;
 
     private String coverImage;
+
+    private Long categoryId;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
