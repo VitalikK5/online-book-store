b7e26e994093423ebc1ac48b8f2a8d8d6050e1d9
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-10 18:34:58 +0200

    Finish hw3

--- src/test/java/com/example/bookhub/BookHubApplicationTests.java
+++ src/test/java/com/example/bookhub/BookHubApplicationTests.java
@@ -2,10 +2,8 @@ package com.example.bookhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class BookHubApplicationTests {
     @Test
     void contextLoads() {
