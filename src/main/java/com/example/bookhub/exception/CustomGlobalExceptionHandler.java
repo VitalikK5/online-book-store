f715f05d8912c073ba5c656fe2530df4c7d2e7cc
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-19 02:33:47 +0200

    Finish hw5

--- src/main/java/com/example/bookhub/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/example/bookhub/exception/CustomGlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
