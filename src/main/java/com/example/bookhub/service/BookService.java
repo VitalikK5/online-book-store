00b334491e9b9459b58ec74e10337e009b112279
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-21 01:57:30 +0200

    Finish hw6

--- src/main/java/com/example/bookhub/service/BookService.java
+++ src/main/java/com/example/bookhub/service/BookService.java
@@ -3,9 +3,9 @@ package com.example.bookhub.service;
 import com.example.bookhub.dto.BookDto;
 import com.example.bookhub.dto.BookSearchParameters;
 import com.example.bookhub.dto.CreateBookRequestDto;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
