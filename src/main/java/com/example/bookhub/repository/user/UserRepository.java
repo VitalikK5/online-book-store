31c821940f7b7528619e329bb519d1a162f36e5e
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-25 19:26:11 +0200

    Finish hw8

--- src/main/java/com/example/bookhub/repository/user/UserRepository.java
+++ src/main/java/com/example/bookhub/repository/user/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.bookhub.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
