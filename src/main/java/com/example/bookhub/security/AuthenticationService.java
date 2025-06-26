d4697e3bda258bd8930ab6faf4e7b8ce9fba3d23
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-06-26 18:43:36 +0200

    Finish hw9

--- src/main/java/com/example/bookhub/security/AuthenticationService.java
+++ src/main/java/com/example/bookhub/security/AuthenticationService.java
@@ -2,13 +2,11 @@ package com.example.bookhub.security;
 
 import com.example.bookhub.dto.user.UserLoginRequestDto;
 import com.example.bookhub.dto.user.UserLoginResponseDto;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @Service
@@ -16,8 +14,7 @@ public class AuthenticationService {
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
 
-    public UserLoginResponseDto authenticate(
-            @RequestBody @Valid UserLoginRequestDto loginRequestDto) {
+    public UserLoginResponseDto authenticate(UserLoginRequestDto loginRequestDto) {
         final Authentication authenticate = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequestDto.email(),
